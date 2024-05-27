15d1b61887cc805d2debac1dfb24e853b1abf7e3
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-05-27 22:07:21 +0530

    error handle, debugging

--- frontend/src/components/HelperHeader.tsx
+++ frontend/src/components/HelperHeader.tsx
@@ -25,16 +25,27 @@ import {
 import { RootState } from "@/redux/store";
 import { handleError } from "@/utils/handleErrors";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 export default function HelperHeader() {
   const [saveLoading, setSavaLoading] = useState<boolean>(false);
+  const [shareBtn, setShareBtn] = useState<boolean>(false);
   const navigate = useNavigate();
   const fullCode = useSelector(
     (state: RootState) => state.compilerSlice.fullCode
   );
+
+  const { urlId } = useParams();
+  useEffect(() => {
+    if (urlId) {
+      setShareBtn(true);
+    } else {
+      setShareBtn(false);
+    }
+  }, [urlId]);
+
   const handleSaveCode = async () => {
     setSavaLoading(true);
     try {
@@ -65,39 +76,44 @@ export default function HelperHeader() {
           <Save />
           {saveLoading ? "Saving..." : "Save"}
         </Button>
-        <Dialog>
-          <DialogTrigger className="whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 flex justify-center items-center gap-1">
-            <Share2Icon fontSize={16} /> Share
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle className="flex gap-1 justify-center items-center"><Code/>Share your code</DialogTitle>
-              <DialogDescription className="flex flex-col gap-2">
-              <input
-                      type="text"
-                      disabled
-                      className="w-full p-2 rounded bg-slate-800 text-slate-400 select-none"
-                      value={window.location.href}
-                    />
-                     <Button
-                      variant="outline"
-                      className="h-full"
-                      onClick={() => {
-                        window.navigator.clipboard.writeText(
-                          window.location.href
-                        );
-                        toast("URL Copied to your clipboard!");
-                      }}
-                    >
-                      <Copy size={14} />
-                    </Button>
-              <p className="text-center text-slate-400 text-xs">
+        {shareBtn && (
+          <Dialog>
+            <DialogTrigger className="whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 flex justify-center items-center gap-1">
+              <Share2Icon fontSize={16} /> Share
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle className="flex gap-1 justify-center items-center">
+                  <Code />
+                  Share your code
+                </DialogTitle>
+                <DialogDescription className="flex flex-col gap-2">
+                  <input
+                    type="text"
+                    disabled
+                    className="w-full p-2 rounded bg-slate-800 text-slate-400 select-none"
+                    value={window.location.href}
+                  />
+                  <Button
+                    variant="outline"
+                    className="h-full"
+                    onClick={() => {
+                      window.navigator.clipboard.writeText(
+                        window.location.href
+                      );
+                      toast("URL Copied to your clipboard!");
+                    }}
+                  >
+                    <Copy size={14} />
+                  </Button>
+                  <p className="text-center text-slate-400 text-xs">
                     Share this URL with your friends to collaborate.
                   </p>
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        )}
       </div>
       <div className="__tab_switcher flex justify-center items-center gap-1">
         <Select
